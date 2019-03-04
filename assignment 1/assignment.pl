383e7f931339042cfa91d61b8d73e314f0ffc33c
Henry <HenryQuan@users.noreply.github.com>
2019-03-04 13:45:04 +1100

    add header comment and prepare for Q2

--- assignment 1/assignment.pl
+++ assignment 1/assignment.pl
@@ -1,4 +1,9 @@
-% Handle nothing or one element
+/*
+    This is the first assignment of COMP3411 T1
+    Yiheng Quan Z5098268
+*/
+
+% Q1, Handle nothing or one element
 sumsq_even([], 0).
 
 % Loop through the list and sum the square of even numbers
@@ -9,3 +14,11 @@ sumsq_even([H|T], EvenSum) :-
 sumsq_even([H|T], Sum) :-
     sumsq_even(T, Sum),
     1 is H mod 2.
+
+% Q2, 
+
+% Q3, 
+
+% Q4,
+
+% Q5, 
