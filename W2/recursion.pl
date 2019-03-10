16e6a7a05ba6a2b774d25b68743509f9276c74a2
Henry <HenryQuan@users.noreply.github.com>
2019-03-10 20:21:07 +1100

    how does recursion work?

--- W2/recursion.pl
+++ W2/recursion.pl
@@ -1,5 +1,8 @@
+% how does this work?
+% This is to define what is a descendant
 descendant(Person, Descendant) :-
     parent(Person, Descendant).
+% This is recursion to find all descendants
 descendant(Person, Descendant) :-
     parent(Person, Child),
     descendant(Child, Descendant).
