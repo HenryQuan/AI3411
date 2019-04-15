942610523a0440584fa7b5d19bb89388302af2c2
Yiheng (CSE) <y.quan@unsw.edu.au>
2019-04-15 19:42:35 +1000

    how I will solve this

--- Assignment 3/agent.py
+++ Assignment 3/agent.py
@@ -3,8 +3,13 @@
 '''
 COMP3411 19T1
 Assignment 3 Nine-board Tic-Tac-Toe
-
 Yiheng Quan Z5098268
+
+I checked Zac senpai's starter code and got some ideas about what's going on.
+My current solution to this problem is to use IDA* and with a limit depth to search from.
+By using minimax, alpha-beta pruning and a limited depth, the program can run faster.
+
+The heuristic function will be measuring 'most wins' and the cost is always one for all moves.
 '''
 
 print('Hello World')
