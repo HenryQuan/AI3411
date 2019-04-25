1a84a2c6fd87ba2ab60be0bf874712a0d5c0199f
Yiheng (CSE) <y.quan@unsw.edu.au>
2019-04-25 11:30:59 +1000

    copy main and parse

--- Assignment 3/agent.py
+++ Assignment 3/agent.py
@@ -11,7 +11,65 @@ My current solution to use minimax and alpha-beta pruning. Gradually increas eth
 The heuristic function will be measuring 'most wins' and the cost is always one for all moves.
 '''
 
-print('Hello World')
-
+'''
 init, start(), second_move(), third_move(),
 last_move(), win(), loss(), draw(), end()
+'''
+
+import socket
+import sys
+
+# Parse command from server (modified from Zac senpai's code)
+def parse(string):
+    if "(" in string:
+        command, args = string.split("(")
+        args = args.split(")")[0]
+        args = args.split(",")
+    else:
+        command, args = string, []
+
+    if command == "second_move":
+        place(int(args[0]), int(args[1]), 2)
+        return play()
+    elif command == "third_move":
+        # place the move that was generated for us
+        place(int(args[0]), int(args[1]), 1)
+        # place their last move
+        place(curr, int(args[2]), 2)
+        return play()
+    elif command == "next_move":
+        place(curr, int(args[0]), 2)
+        return play()
+    elif command == "win":
+        print("Yay!! We win!! :)")
+        return -1
+    elif command == "loss":
+        print("We lost :(")
+        return -1
+    return 0
+
+# Setup socket and keep receiving commands (modified from Zac senpai's code)
+def main():
+    if (len(sys.argv) < 3 or (len(sys.argv) > 2 and not sys.argv[1] == '-p')):
+        # some basic validations
+        print("Usage: ./agent.py -p (port)")
+        sys.exit(1)
+
+    port = int(sys.argv[2]) # Usage: ./agent.py -p (port)
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+    s.connect(('localhost', port))
+    while True:
+        text = s.recv(1024).decode()
+        if not text:
+            continue
+        for line in text.split("\n"):
+            response = parse(line)
+            if response == -1:
+                s.close()
+                return
+            elif response > 0:
+                s.sendall((str(response) + "\n").encode())
+
+if __name__ == "__main__":
+    main()
